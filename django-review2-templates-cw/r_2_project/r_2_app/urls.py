729b927c6dc0825715cf5aa910cf11e2ccf43552
ChelsGreg <46455520+ChelsGreg@users.noreply.github.com>
2019-02-26 16:07:05 -0600

    Update urls.py

--- django-review2-templates-cw/r_2_project/r_2_app/urls.py
+++ django-review2-templates-cw/r_2_project/r_2_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-
+<!--ROUTING PATHS TO FUNCTIONS IN VIEWS-->
 urlpatterns = [
     path('songs/', views.list_songs, name='list_songs'),
     path('songs/<int:song_id>/', views.list_song, name='song_details'),
